0a1c9a711da2dd6a74ff352217643d61bd21afd4
Namika <loud.music.n@gmail.com>
2025-05-28 20:51:44 +0900

    - create feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -32,23 +32,46 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
 }
 
 export async function createFeedback(params: CreateFeedbackParams) {
-  const { interviewI, userId, transcript } = params;
+  const { interviewId, userId, transcript } = params;
 
   try {
     const formattedTranscript = transcript.map((sentence: { role: string; content: string; }) => (
       `- ${ sentence.role }: ${sentence.content}\n`
     )).join('');
 
-    const { object } = await generateObject({
-      model: google('gemini-2.0-flash-001', {
+    const { object: { totalScore, categoryScores, strengths, areasForImprovement, finalAssessment } } = await generateObject({
+      model: google("gemini-2.0-flash-001", {
         structuredOutputs: false,
       }),
       schema: feedbackSchema,
-      prompt: 'ill give you the transcript and you asses the candidate',
-      system: 'You are a professional interviewer.'
+      prompt: `
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
+      system:
+        "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
     });
 
-    return { success: true, feedbackId: "" };
+    const feedback = await db.collection('feedback').add({
+      interviewId: interviewId,
+      userId,
+      totalScore,
+      categoryScores,
+      strengths,
+      areasForImprovement,
+      finalAssessment,
+      createdAt: new Date().toISOString(),
+    })
+
+    return { success: true, feedbackId: feedback.id };
 
   } catch(e) {
     console.error('Error saving feedback', e)
